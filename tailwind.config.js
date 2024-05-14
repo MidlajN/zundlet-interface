189be3f5af583ce196b1d7e2726004d394b41d99
MidlajN <midlaj4n@gmail.com>
2024-05-14 12:44:46 +0530

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,23 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: '1.5rem',
+        lg: '2rem',
+        xl: '2.5rem',
+        '2xl': '3rem',
+      }
     },
+    // extend: {
+    //   backgroundImage: {
+    //     "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+    //     "gradient-conic":
+    //       "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    //   },
+    // },
   },
   plugins: [],
 };
